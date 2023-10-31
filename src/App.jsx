07740d3bc82fb31e2d6ec553ab9477efef2947cd
Osama7erzallah <osamaherzallah.2002@gmail.com>
2023-10-31 14:02:49 +0200

    react_HW3_part1

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Home from './components/home/Home.jsx'
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+  
+<Home/>
+    </>
+  )
+}
+
+export default App
---
+++ src/components/products/Product.jsx
@@ -0,0 +1,15 @@
+import React from 'react'
+
+
+
+
+export default function Product() {
+    
+    const getData=(name)=>{
+    console.log(name);
+        
+    }
+  return (
+    <div>Product</div>
+  )
+}
